d24da792810547351649c6dd6469b7ea20684a21
conformist-mw <conformist.mw@gmail.com>
2017-04-24 13:41:38 +0300

    added sleep

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-from time import sleep
 import unittest
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
