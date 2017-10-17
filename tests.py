fbc03aa52caebf673e839a4215660f4eb637e34c
conformist-mw <conformist.mw@gmail.com>
2017-10-17 15:08:09 +0300

    use native driverwait instead of time.sleep

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 import unittest
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,6 +8,7 @@ PHANTOMJS_PATH = './node_modules/phantomjs-prebuilt/bin/phantomjs'
 CREATE_USER_URL = 'http://atomicboard.devman.org/create_test_user/'
 URL = 'http://atomicboard.devman.org/'
 TEXT_FOR_TEST = 'test_test_test'
+TEST_TASK_HEADING = 'test_task_heading'
 
 
 class AtomicBoardTest(unittest.TestCase):
@@ -25,39 +25,42 @@ class AtomicBoardTest(unittest.TestCase):
     def test_whole_page_loaded(self):
         assert self.driver.find_element(By.CLASS_NAME, 'js-tickets-column')
 
-    def test_edit_exist_task(self):
+    def test_edit_existing_task(self):
         self.driver.find_element(By.CSS_SELECTOR, 'span.editable').click()
         task_input = self.driver.find_element(By.CLASS_NAME, 'editable-input')
         task_input.clear()
         task_input.send_keys(TEXT_FOR_TEST)
         self.driver.find_element(By.CLASS_NAME, 'glyphicon-ok').click()
         tasks = self.driver.find_elements(By.CLASS_NAME, 'editable')
-        test_text = [x.text for x in tasks if x.text == TEXT_FOR_TEST][0]
-        assert test_text == TEXT_FOR_TEST
+        assert any([task.text for task in tasks if task.text == TEXT_FOR_TEST])
 
     def test_mark_task_closed(self):
         tasks = self.driver.find_elements(By.CLASS_NAME, 'ticket_status')
         open_task = [task for task in tasks if task.text == 'open'][0]
         open_task.click()
-        sleep(5)
-        buttons = self.driver.find_elements(By.CLASS_NAME,
-                                            'change-status-form__button')
-        [but for but in buttons if but.text == 'closed'][0].click()
+        WebDriverWait(self.driver, 35).until(
+            EC.element_to_be_clickable((By.CLASS_NAME,
+                                        'change-status-form__button')))
+        self.driver.find_element(
+            By.CSS_SELECTOR,
+            'button.btn-primary.change-status-form__button').click()
         assert open_task.text == 'closed'
 
     def test_add_new_task(self):
-        add_span = self.driver.find_element(By.CLASS_NAME,
-                                            'add-ticket-block_button')
-        add_span.click()
+        self.driver.find_element(By.CLASS_NAME,
+                                 'add-ticket-block_button').click()
         input_field = self.driver.find_element(By.CLASS_NAME, 'editable-input')
-        input_field.send_keys('new_task_added')
+        input_field.send_keys(TEST_TASK_HEADING)
         self.driver.find_element(By.CLASS_NAME, 'glyphicon-ok').click()
-        sleep(5)
+        WebDriverWait(self.driver, 25).until(
+            EC.invisibility_of_element_located((By.CSS_SELECTOR,
+                                                'input.editable-input')))
         tasks = self.driver.find_elements(By.CLASS_NAME, 'editable')
-        assert len(
-            [task for task in tasks if task.text == 'new_task_added']) == 1
+        assert any([task for task in tasks if task.text == TEST_TASK_HEADING])
 
     def test_drag_and_drop_task(self):
+        # This test running with drug_n_drop_helper.js script
+        # check the SO answer https://stackoverflow.com/a/29381532/3355831
         cols = self.driver.find_elements(By.CLASS_NAME, 'tickets-column')
         task = cols[0].find_element(By.CLASS_NAME, 'js-ticket')
         task_text = task.text
@@ -66,7 +69,6 @@ class AtomicBoardTest(unittest.TestCase):
         self.driver.execute_script(
             drag_and_drop_js + ('$("div.js-ticket:eq(0)").simulateDragDrop('
                                 '{dropTarget: "span.tickets-column:eq(1)"});'))
-
         target_text = cols[1].find_element(By.CLASS_NAME, 'js-ticket').text
         assert task_text == target_text
 
